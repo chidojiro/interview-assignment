b97732abad61879f2511278016a2745f816da551
Petyo Stoyanov <petyosv@gmail.com>
2019-08-29 07:58:45 +0300

    Update upload field

--- src/components/form-elements/Upload.js
+++ src/components/form-elements/Upload.js
@@ -6,7 +6,26 @@ const Upload = ({type, placeholder, ...props}) => {
     ...props,
     type: 'file'
   };
-  return <input {...fieldProps} />;
+  return (
+    <div class="upload" data-rs-upload="">
+      <input {...fieldProps} />
+      <div class="upload__content">
+        <div class="upload__text">
+          <span class="icon icon--inline fill--dark-blue-50">
+            <svg>
+              <use xlinkHref="human-forward/assets/image/icons.svg#attachment" />
+            </svg>
+          </span>
+          <span class="upload__add">add files</span>
+          <span class="text--alternative hidden--until-l ">or drop files here</span>
+        </div>
+        <p class="text--alternative">.pdf, .docx, .jpg / max. 10 mb</p>
+      </div>
+      <div class="upload__content upload__content--drop">
+        <span>drop files here</span>
+      </div>
+    </div>
+  );
 }
 
 Upload.displayName = 'Upload'
