960435e992c07720555ecac99d84507df67880e2
Petyo Stoyanov <petyosv@gmail.com>
2019-09-09 01:06:17 +0300

    Update

--- src/components/layout/Block.js
+++ src/components/layout/Block.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Block = ({label, children, className, ...props}) => {
+const Block = ({label, children, className, size, align, ...props}) => {
   return (
-    <div className={`block ${className||""}`}>
+    <div className={`block ${className||""} ${size && `block--${size}`}`}>
       <div className="block__wrapper wrapper">
         {label && (
           <div className="block__header">
             <h2 className="block__title">{label}</h2>
           </div>
         )}
-        <div className="block__content">
+        <div className={`block__content ${size && `block__content--${size}`} ${align && `block__content--align-${align}`}`}>
           {children}
         </div>
       </div>
