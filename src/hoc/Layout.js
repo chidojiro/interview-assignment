05e00de36eba627ae0d5f04a280a47f7a849ace3
Petyo Stoyanov <petyosv@gmail.com>
2019-09-15 15:17:37 +0300

    Update

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -50,7 +50,7 @@ const Layout = ({registry}) => ChildComponent => {
             <div className={`header__content content-block ${!media && "header__content--full-width"}`}>
               {greeting && (
                 <p className="content-block__eyebrow text--alternative">
-                  <span ref={addCustomGreetings} />
+                  <span  />
                   , {greeting}
                 </p>
               )}
