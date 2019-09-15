40d2b4f2ce2865e1fee863fb76aa25d918ea47cf
Petyo Stoyanov <petyosv@gmail.com>
2019-09-15 15:43:36 +0300

    Update

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -23,7 +23,7 @@ const Layout = ({registry}) => ChildComponent => {
     const SubMenu = registry.getComponent('menu', templateSuggestions.subMenu);
     const Breadcrumbs = registry.getComponent('layout', templateSuggestions.breadcrumbs);
 
-    const { media, headline, subtitle, brand, greeting } = header || {};
+    const { media, headline, subtitle, brand, greeting, size } = header || {};
 
     const addCustomGreetings = element => {
       if (!element) return;
@@ -33,7 +33,7 @@ const Layout = ({registry}) => ChildComponent => {
     return (
       <div>
         {ChildComponent.Head && <ChildComponent.Head title={title} {...props} />}
-        <header className={`header bg-brand--${brand || 'dark-blue'}`}>
+        <header className={`header bg-brand--${brand || 'dark-blue'} ${size && `header--${size}`}`}>
           <div className="platform-placeholder platform-placeholder--navigation text--alternative">
             <div className={`bg-brand--${brand || 'dark-blue'}`}>
               <div className="navigation">
