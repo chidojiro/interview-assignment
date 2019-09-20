1571bc20800c2e5a57d796b446d4742f4dd0e615
Petyo Stoyanov <petyosv@gmail.com>
2019-09-20 17:30:07 +0300

    Update

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -4,7 +4,6 @@ import {CustomGreeting} from '../orbit/custom-greeting';
 const Layout = ({registry}) => ChildComponent => {
 
   const TopNavigation = registry.getComponent('layout', 'top-navigation');
-  const Footer = registry.getComponent('layout', 'footer');
 
   const Component = ({title, header, breadcrumbs, authenticated, ...props}) => {
 
@@ -12,6 +11,7 @@ const Layout = ({registry}) => ChildComponent => {
       subMenu: ['sub'],
       breadcrumbs: ['breadcrumbs'],
       footerNav: ['footer-navigation'],
+      footer: ['footer'],
     };
 
     for (let key in templateSuggestions) {
@@ -21,6 +21,7 @@ const Layout = ({registry}) => ChildComponent => {
     }
 
     const FooterNavigation = registry.getComponent('layout', templateSuggestions.footerNav);
+    const Footer = registry.getComponent('layout', templateSuggestions.footer);
     const SubMenu = registry.getComponent('menu', templateSuggestions.subMenu);
     const Breadcrumbs = registry.getComponent('layout', templateSuggestions.breadcrumbs);
 
