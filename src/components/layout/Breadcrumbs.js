83d1f65389399a5f9b82ca1058ad1fe3b0a39d82
Petyo Stoyanov <petyosv@gmail.com>
2019-09-30 18:48:12 +0300

    Update

--- src/components/layout/Breadcrumbs.js
+++ src/components/layout/Breadcrumbs.js
@@ -1,17 +1,24 @@
 import Reach from "react";
 
-const Breadcrumbs = ({ breadcrumbs }) => (
-  <nav className="breadcrumb" aria-label="breadcrumb">
-    <a href="#" className="breadcrumb__link hidden--from-l">one level higher</a>
-    <ul className="breadcrumb__list hidden--until-l">
-      {breadcrumbs.map((step, index) => (
-        <li className="breadcrumb__item" key={index}>
-          {step.uri && <a href={step.uri} className="breadcrumb__link">{step.title}</a>}
-          {!step.uri && step.title}
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Breadcrumbs = ({ breadcrumbs }) => {
+  let go_back;
+
+  if (breadcrumbs.length > 1) {
+    go_back = breadcrumbs[breadcrumbs.length - 2];
+  }
+  return (
+    <nav className="breadcrumb" aria-label="breadcrumb">
+      {go_back && <a href={go_back.uri} className="breadcrumb__link hidden--from-l">{go_back.title}</a>}
+      <ul className="breadcrumb__list hidden--until-l">
+        {breadcrumbs.map((step, index) => (
+          <li className="breadcrumb__item" key={index}>
+            {step.uri && <a href={step.uri} className="breadcrumb__link">{step.title}</a>}
+            {!step.uri && step.title}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+};
 
 export default Breadcrumbs;
