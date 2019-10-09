4edafdb37a824ca881fd28528862f32f5e982100
Petyo Stoyanov <petyosv@gmail.com>
2019-10-09 10:27:16 +0300

    Update

--- src/components/form-elements/Dropdown.js
+++ src/components/form-elements/Dropdown.js
@@ -2,13 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import withFieldGroup from "./FieldGroup";
 
-const Dropdown = ({options, icon, required, ...props}) => {
+const Dropdown = ({options, icon, required, onChange, ...props}) => {
   const iconProps = {
     xlinkHref: "human-forward/assets/image/icons.svg#chevron-down"
   }
+
+  const updateValue = event => {
+    if (event.target.value === '_none' || event.target.value === null || event.target.value === '') {
+      if (!event.target.classList.contains('untouched')) {
+        event.target.classList.add('untouched');
+      }
+    } else if (event.target.classList.contains('untouched')) {
+      event.target.classList.remove('untouched');
+    }
+    onChange(event);
+  }
+
   return (
     <React.Fragment>
-      <select className="untouched" data-rs-untouched="" {...props}>
+      <select className="untouched" data-rs-untouched="" onChange={updateValue} {...props}>
         {options.map((opt,index) => <option key={index} value={opt.value}>{opt.label}</option>)}
       </select>
       {icon && (
