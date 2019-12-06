fae7ee1f16bac2ce97f90f29bec6d353daacad29
ppetkov <petko.petkov@ffwagency.com>
2019-12-06 15:33:03 +0200

    RBD-2724: Daytime greetings not translated - Convert greeting to react component

--- build/080ee1e71f28b72ce19c.js
+++
@@ -1,1 +0,0 @@
-exports.ids=[14],exports.modules={131:function(e,t,n){"use strict";n.r(t);var r=n(39),a=n.n(r),l=a.a.createElement;t.default=function(e){var t=e.greeting;return l(a.a.Fragment,null,t&&l("p",{className:"content-block__eyebrow text--alternative"},l("span",null),"morning , ",t))}}};
