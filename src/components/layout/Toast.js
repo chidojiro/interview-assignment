a3bb6e330e6c7a33dea857bc18055607c75babfa
ppetkov <petko.petkov@ffwagency.com>
2020-05-13 14:19:56 +0300

    extend toast with links

--- src/components/layout/Toast.js
+++ src/components/layout/Toast.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-const Toast = ({message, onClose, show, icon}) => {
+const Toast = ({ message, onClose, show, icon, links = [] }) => {
   const [active, setActive] = useState(show);
 
   useEffect(() => {
@@ -19,6 +19,14 @@ const Toast = ({message, onClose, show, icon}) => {
   return (
     <div className={classes.join(' ')} data-rs-toast="3" data-rs-toast-3="">
       <p className="toast__message">{message}</p>
+      {links && <div className="toast__cta">
+        {links.map((link, key) => {
+          const { text, classes = [], ...attr } = link;
+          classes.push('toast__cta-link')
+
+          return <a {...attr} key={key} className={classes.join(" ")}>{text}</a>
+        })}
+      </div>}
       <button className="button--icon-only" data-rs-closable="data-rs-toast-3" onClick={onClose}>
         {icon && (
           <span className="icon fill--dark-blue-50">
