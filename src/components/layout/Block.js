974545f463728e9a3b3a4bbc12bf0c46fd4bf3d6
ppetkov <petko.petkov@ffwagency.com>
2020-05-19 17:00:30 +0300

    Add block control

--- src/components/layout/Block.js
+++ src/components/layout/Block.js
@@ -1,20 +1,42 @@
 import React from "react";
 
-const Block = ({label, children, className, size, align, ...props}) => {
+const Block = ({
+  label,
+  children,
+  className,
+  size,
+  align,
+  blockControl,
+  ...props
+}) => {
   return (
-    <div className={`block ${className||""} ${size && `block--${size}`}`}>
+    <div className={`block ${className || ""} ${size && `block--${size}`}`}>
       <div className="block__wrapper wrapper">
         {label && (
           <div className="block__header">
             <h2 className="block__title">{label}</h2>
+            {blockControl && (
+              <a href="#" onClick={blockControl.cb} class="block__control">
+                <span class="icon icon--inline fill-brand--blue">
+                  <svg>
+                    <use xlinkHref={`${blockControl.path}/icons.svg#add`}></use>
+                  </svg>
+                </span>
+                {blockControl.label}
+              </a>
+            )}
           </div>
         )}
-        <div className={`block__content ${size && `block__content--${size}`} ${align && `block__content--align-${align}`}`}>
+        <div
+          className={`block__content ${size && `block__content--${size}`} ${
+            align && `block__content--align-${align}`
+          }`}
+        >
           {children}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Block;
