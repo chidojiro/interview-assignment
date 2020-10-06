d857ac5abd45e7f44b96be4d06efc78125e4eb22
ppetkov <petko.petkov@ffwagency.com>
2020-10-06 14:25:02 +0300

    fix breadcrumbs

--- src/components/layout/Breadcrumbs.js
+++ src/components/layout/Breadcrumbs.js
@@ -7,17 +7,19 @@ const Breadcrumbs = ({ breadcrumbs }) => {
     go_back = breadcrumbs[breadcrumbs.length - 2];
   }
   return (
-    <nav className="breadcrumb" aria-label="breadcrumb">
-      {go_back && <a href={go_back.uri} className="breadcrumb__link hidden--from-l">{go_back.title}</a>}
-      <ul className="breadcrumb__list hidden--until-l">
-        {breadcrumbs.map((step, index) => (
-          <li className="breadcrumb__item" key={index}>
-            {step.uri && <a href={step.uri} className="breadcrumb__link">{step.title}</a>}
-            {!step.uri && step.title}
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className="navigation__bottom">
+      <nav className="breadcrumb" aria-label="breadcrumb">
+        {go_back && <a href={go_back.uri} className="breadcrumb__link hidden--from-l">{go_back.title}</a>}
+        <ul className="breadcrumb__list hidden--until-l">
+          {breadcrumbs.map((step, index) => (
+            <li className="breadcrumb__item" key={index}>
+              {step.uri && <a href={step.uri} className="breadcrumb__link">{step.title}</a>}
+              {!step.uri && step.title}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   )
 };
 
