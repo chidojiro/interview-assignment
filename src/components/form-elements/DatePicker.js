af2d94cf00968297ad578da1803ad95b4a137e0b
ppetkov <petko.petkov@ffwagency.com>
2020-11-17 16:05:17 +0200

    update datepicker markup.

--- src/components/form-elements/DatePicker.js
+++ src/components/form-elements/DatePicker.js
@@ -6,11 +6,11 @@ const DatePicker = ({orbitLib, lang="", iconPath, onChange, name, defaultValue, 
   const ref = useRef();
   const fieldProps = {
     type: "text",
-    "data-rs-flat-datepicker-input": "",
-    "data-rs-flat-datepicker-default-date": "",
-    "data-rs-flat-datepicker-alt-format": "Y-m-d",
-    "data-rs-flat-datepicker-date-format": "Y-m-d",
-    "data-rs-flat-datepicker-default-date": defaultValue,
+    "data-rs-datepicker-input": "",
+    "data-rs-datepicker-default-date": "",
+    "data-rs-datepicker-alt-format": "Y-m-d",
+    "data-rs-datepicker-date-format": "Y-m-d",
+    "data-rs-datepicker-default-date": defaultValue,
     name,
     ...props,
   };
@@ -18,14 +18,14 @@ const DatePicker = ({orbitLib, lang="", iconPath, onChange, name, defaultValue, 
   useEffect(()=> {
     if (!ref.current && !orbitLib) return;
 
-    new orbitLib.FlatDatePicker(ref.current)
+    new orbitLib.DatePicker(ref.current)
 
     // Handle change event. Flat picker use different field to write the value
     window.addEventListener('onFlatDatePickerChange', () => {
       if (!ref.current) return;
 
       const target = ref.current.querySelector('input.input');
-      // Target name should be same as data-rs-flat-datepicker-input input name to allow on change event.
+      // Target name should be same as data-rs-datepicker-input input name to allow on change event.
       target.name = name;
       onChange({
         target: target
@@ -34,13 +34,13 @@ const DatePicker = ({orbitLib, lang="", iconPath, onChange, name, defaultValue, 
   }, [])
 
   return (
-    <div ref={ref} data-rs-flat-datepicker="">
+    <div ref={ref} data-rs-datepicker="">
       <button className="button--clean" type="button"></button>
       <input
         placeholder="select date..."
         tabIndex="-1"
         className="flatpickr"
-        data-rs-flat-datepicker-language={lang}
+        data-rs-datepicker-language={lang}
         {...fieldProps}
       />
       <span className="button--icon-only button--form-group-style">
