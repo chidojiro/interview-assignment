62ef222de23e39eba4a4acc7d31a29fb743b6c6a
zafer.genc <zafer.genc@ausy.pt>
2022-09-15 11:55:40 +0100

    RBD-13726: Add TT logo.

--- src/components/navigation/Logo.js
+++ src/components/navigation/Logo.js
@@ -1,12 +1,14 @@
 import React from "react";
 import LogoDefault from "./themes/LogoDefault";
 import LogoSph from "./themes/LogoSph";
+import LogoTT from "./themes/LogoTT";
 import t from "prop-types";
 
 const Logo = ({ theme, homepageUrl }) => {
   return (
     <a className={"logo"} href={homepageUrl}>
-      {theme == "sph" ? <LogoSph /> : <LogoDefault />}
+      {theme == "sph" ? <LogoSph /> :
+       theme == "tt" ? <LogoTT /> : <LogoDefault />}
     </a>
   );
 };
---
+++ src/components/navigation/themes/LogoTT.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const LogoTT = () => <img src="/themes/custom/tt/src/assets/img/logo.png" width="170" height="36" alt="Tempoteam" />;
+
+export default LogoTT;
