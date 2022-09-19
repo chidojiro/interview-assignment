cdf44e1a69ee0033acd498bfca430d30ba7d635c
zafer.genc <zafer.genc@ausy.pt>
2022-09-19 08:46:56 +0100

    RBD-13726: Change TT logo's alt text.

--- src/components/navigation/themes/LogoTT.js
+++ src/components/navigation/themes/LogoTT.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const LogoTT = () => <img src="/themes/custom/tt/src/assets/img/logo.png" width="170" height="36" alt="Tempoteam" />;
+const LogoTT = () => <img src="/themes/custom/tt/src/assets/img/logo.png" width="170" height="36" alt="Tempo-Team" />;
 
 export default LogoTT;
