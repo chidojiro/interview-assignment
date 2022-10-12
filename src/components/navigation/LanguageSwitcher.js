6d3b286cabd7e56af0b0c2f4707f5e90d518e4d0
Alexander Mikhutkin <alexander.mikhutkin@propeople.com.ua>
2022-10-12 17:31:31 +0300

    RXPIP-3316: Revert LanguageSwitcher functionality back to links.

--- src/components/navigation/LanguageSwitcher.js
+++ src/components/navigation/LanguageSwitcher.js
@@ -1,44 +1,31 @@
 import React from "react";
 import t from "prop-types";
 
-const LanguageSwitcher = ({ items }) => {
+const LanguageSwitcher = ({ items, extraClasses }) => {
   if (!items || (items && items.length < 2)) {
     return null;
   }
 
   return (
-    <li className="navigation__service-item language__dropdown__wrapper">
-      <a className="language__dropdown navigation__service-link">
-        <span className="icon icon--language">
-          <svg>
-            <use xlinkHref="/themes/custom/bluex/dist/assets/image/icons.svg#globe"></use>
-          </svg>
-        </span>
-        <select className="js-language-switcher" name="language-switcher">
-          {items.map((lang) => {
-            return (
-              <option
-                key={lang.language}
-                value={lang.language}
-                selected={lang.isActive}
-                data-url={lang.url}>
-                {lang.language}
-              </option>
-            );
-          })}
-        </select>
-        <span className="icon select--status">
-          <svg>
-            <use xlinkHref="/themes/custom/bluex/dist/assets/image/icons.svg#chevron-down-8"></use>
-          </svg>
-        </span>
-      </a>
-    </li>
+    <ul className={`top-link language-switcher ${extraClasses ? extraClasses : ""}`}>
+      {items.map((lang) => (
+        <li key={lang.language} className="top-link__item">
+          {!lang.isActive ? (
+            <a href={lang.url} hrefLang={lang.language} className="language-link">
+              {lang.language}
+            </a>
+          ) : (
+            lang.language
+          )}
+        </li>
+      ))}
+    </ul>
   );
 };
 
 LanguageSwitcher.propTypes = {
   items: t.array,
+  extraClasses: t.string,
 };
 
 export default LanguageSwitcher;
