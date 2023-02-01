4547341ceb4b5e7d9d809fe596460f8338038fbf
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-02-01 15:54:35 +0200

    RXPIP-7574: Changing fetch assets due to the nodejs 18 version.

--- src/docs-lib/fetchAsset.js
+++ src/docs-lib/fetchAsset.js
@@ -5,8 +5,8 @@ const path = require("path");
 const pathToAssets = path.resolve(__dirname, "./assets");
 !fs.existsSync(pathToAssets) && fs.mkdirSync(pathToAssets);
 
-function fetchAsset(source, dist) {
-  fetch(source)
+function fetchAsset(source, headers, dist) {
+  fetch(source, headers)
     .then(function (response) {
       if (response.status >= 400) {
         throw new Error("Bad response from server");
