5a07c0ac1bef6d9b18b3e9352b8d7c1b8fc841f4
Radoslav Radoychev <radoslav.radoychev@ffwagency.com>
2023-02-09 15:40:43 +0200

    RXPIP-7027: PasswordField refactored to receive prop for showing and hiding the validation rules below the field. Fixed ESLint rules and Types.

--- src/components/forms/PasswordField.tsx
+++ src/components/forms/PasswordField.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import withField from "../../hoc/withField";
-import useLibrary from "../../hooks/useLibrary";
-import FormGroup from "../form-group/FormGroup";
-import Label from "../form-group/Label";
-import Svg from "../base/Svg";
+import withField from '../../hoc/withField';
+import useLibrary from '../../hooks/useLibrary';
+import FormGroup from '../form-group/FormGroup';
+import Label from '../form-group/Label';
+import Svg from '../base/Svg';
 
 interface PasswordFieldProps {
   name: string;
@@ -23,9 +23,10 @@ interface PasswordFieldProps {
     useChar: string;
     noSymbol: string;
   };
+  hideValidationList?: boolean;
   /** Refers to the aria-label attribute of the show button. */
   buttonLabel?: string;
-  forgottenPasswordLink?: any;
+  forgottenPasswordLink?: React.ReactElement;
   /** Used to pass js Orbit library responsible for functionality. Note: This should passed on component setup so you don't have to pass it every time. */
   libs?: [];
   /** @ignore Private props from HOC for easy setup. */
@@ -42,26 +43,27 @@ interface PasswordFieldProps {
  * A field to enter data in a pre defined format. See [here](https://randstad.design/components/core/forms/input-field/)
  *
  * ---
- * *Every other passed props will be added to `<input>`. Like "data-attribute" and "aria-label"*
+ * *Every other passed props will get added to `<input>`. Like "data-attribute" and "aria-label"*
  *
- * **Wrapped with `FormGroup` component and support all of its props.**
+ * **Wrapped with `FormGroup` component and supports all the props from it.**
  */
-const PasswordField: React.FC<PasswordFieldProps> = ({
+function PasswordField({
   libs,
   forgottenPasswordLink,
   _formGroupProps = {},
   minChars = 8,
   validationSchema,
-  buttonLabel = "show password",
+  buttonLabel = 'show password',
+  hideValidationList = true,
   ...props
-}) => {
-  const [ref]: any = useLibrary(libs!);
+}: PasswordFieldProps): React.ReactElement {
+  const [ref] = useLibrary(libs ?? []);
 
   const defaultValidationSchema = {
     minSign: `${minChars} characters`,
-    useChar: "1 small letter",
-    useUpper: "1 capital letter",
-    useDigit: "1 number",
+    useChar: '1 small letter',
+    useUpper: '1 capital letter',
+    useDigit: '1 number',
   };
 
   const currentValidationSchema = {
@@ -69,55 +71,56 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
     ...validationSchema,
   };
 
-  let forgottenPasswordWithClass = forgottenPasswordLink ? { ...forgottenPasswordLink } : null;
+  const forgottenPasswordWithClass = forgottenPasswordLink ? { ...forgottenPasswordLink } : null;
 
   if (forgottenPasswordWithClass) {
     forgottenPasswordWithClass.props = {
       ...forgottenPasswordWithClass.props,
-      className: "form-group__action-link",
+      className: 'form-group__action-link',
     };
   }
 
-  const labelWithForgottenPasswordLink = () => {
-    return (
-      <div className="flex justify-between">
-        <Label
-          label={_formGroupProps.label}
-          id={_formGroupProps.id}
-          required={_formGroupProps.required}
-          capitalize={_formGroupProps.capitalize}
-          optionalLabel={_formGroupProps.optionalLabel}
-        />
-        {forgottenPasswordWithClass}
-      </div>
-    );
-  };
+  const labelWithForgottenPasswordLink = () => (
+    <div className="flex justify-between">
+      <Label
+        label={_formGroupProps.label}
+        id={_formGroupProps.id}
+        required={_formGroupProps.required}
+        capitalize={_formGroupProps.capitalize}
+        optionalLabel={_formGroupProps.optionalLabel}
+      />
+      {forgottenPasswordWithClass}
+    </div>
+  );
 
   const forgottenPasswordLinkAttributes = forgottenPasswordLink
     ? {
-        _overrideLabel: labelWithForgottenPasswordLink(),
-      }
+      _overrideLabel: labelWithForgottenPasswordLink(),
+    }
     : {};
 
   return (
     <FormGroup
       {..._formGroupProps}
       _configClasses="form-group--password"
-      _withoutWrapper={true}
-      {...forgottenPasswordLinkAttributes}>
+      _withoutWrapper
+      {...forgottenPasswordLinkAttributes}
+    >
       <div
         className="form-group__input form-group__input--button password-validator"
         data-scl=""
-        ref={ref}
+        ref={ref as React.RefObject<HTMLDivElement>}
         data-rs-password-validator-min-chars={minChars}
         data-rs-password-validator=""
-        data-rs-password-visibility="">
+        data-rs-password-visibility=""
+      >
         <input {...props} type="password" />
         <button
           type="button"
           data-rs-password-visibility-trigger=""
           className="button--icon-only show-password"
-          aria-label={buttonLabel}>
+          aria-label={buttonLabel}
+        >
           <span className="icon">
             <Svg icon="eye" />
           </span>
@@ -126,7 +129,8 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
       <div
         className="password-validator__validate-list"
         data-rs-password-validator-checklist=""
-        hidden>
+        hidden={hideValidationList}
+      >
         <ul>
           {Object.entries(currentValidationSchema).map(([type, schema]) => (
             <li data-rs-password-validator-validate={type} key={type}>
@@ -137,6 +141,6 @@ const PasswordField: React.FC<PasswordFieldProps> = ({
       </div>
     </FormGroup>
   );
-};
+}
 
 export default withField(PasswordField);
