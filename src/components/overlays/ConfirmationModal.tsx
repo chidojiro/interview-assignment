07e7b7c7dcf8abbbad5887ed36da4406b9f4895b
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-02-15 15:29:09 +0200

    RXPIP-8021: Reverted ConfirmationModal conflicts.

--- src/components/overlays/ConfirmationModal.tsx
+++ src/components/overlays/ConfirmationModal.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback } from 'react';
 import classNames from 'classnames';
-// import styles, { keyframes } from 'styled-components';
+import styles, { keyframes } from 'styled-components';
 import Icon from '../Icon';
 import Button from '../button/Button';
 
@@ -15,53 +15,53 @@ interface ConfirmationModalProps {
   onSubmit?: (event: React.MouseEvent) => void;
 }
 
-// const popupEnter = keyframes`
-//   0% { opacity: 0; }
-//   100% { opacity: 1; }
-// `;
-//
-// const popupEnterExit = keyframes`
-//   0% { opacity: 1; }
-//   100% { opacity: 0; }
-// `;
-//
-// const popupEnterDone = keyframes`
-//   0% { transform: translateY(10%) }
-//   100% { transform: none; }
-// `;
-//
-// const popupEnterDoneExit = keyframes`
-//   0% { transform: none; }
-//   100% { transform: translateY(10%) }
-// `;
-//
-// const styleProps = `
-//     animation-duration: 0.3s;
-//     animation-timing-function: ease;
-// `;
-//
-// const ModalStyle = styles.div`
-//   & {
-//     animation-name: ${popupEnter};
-//     ${styleProps}
-//   }
-//
-//   & .modal__dialog {
-//     animation-name: ${popupEnterDone};
-//     ${styleProps}
-//   }
-//
-//   &.modal-exit {
-//     animation-name: ${popupEnterExit};
-//     ${styleProps}
-//   }
-//
-//   &.modal-exit .modal__dialog {
-//     animation-name: ${popupEnterDoneExit};
-//     ${styleProps}
-//   }
-//
-// `;
+const popupEnter = keyframes`
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
+const popupEnterExit = keyframes`
+  0% { opacity: 1; }
+  100% { opacity: 0; }
+`;
+
+const popupEnterDone = keyframes`
+  0% { transform: translateY(10%) }
+  100% { transform: none; }
+`;
+
+const popupEnterDoneExit = keyframes`
+  0% { transform: none; }
+  100% { transform: translateY(10%) }
+`;
+
+const styleProps = `
+    animation-duration: 0.3s;
+    animation-timing-function: ease;
+`;
+
+const ModalStyle = styles.div`
+  & {
+    animation-name: ${popupEnter};
+    ${styleProps}
+  }
+
+  & .modal__dialog {
+    animation-name: ${popupEnterDone};
+    ${styleProps}
+  }
+
+  &.modal-exit {
+    animation-name: ${popupEnterExit};
+    ${styleProps}
+  }
+
+  &.modal-exit .modal__dialog {
+    animation-name: ${popupEnterDoneExit};
+    ${styleProps}
+  }
+
+`;
 
 function ConfirmationModal({
   title = 'are you sure?',
@@ -95,53 +95,53 @@ function ConfirmationModal({
   }, [modalClose]);
 
   return (
-    <React.Fragment>
+    <ModalStyle className="modal modal--active" data-rs-modal="modal" onClick={(event: React.MouseEvent<HTMLDivElement>) => modalClose(event)}>
       {/* Tag <div> needed here according to the Orbit */}
       {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
       <div
         onClick={(event: React.MouseEvent<HTMLDivElement>) => onClickHandler(event)}
-        className='modal__dialog bg-variant-brand-tertiary'
-        role='dialog'
-        aria-modal='true'
-        aria-labelledby='#'
-        data-rs-modal-dialog=''
+        className="modal__dialog bg-variant-brand-tertiary"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="#"
+        data-rs-modal-dialog=""
       >
-        <div className='modal__header' data-rs-modal-header=''>
-          <div className='modal__title'>{title}</div>
+        <div className="modal__header" data-rs-modal-header="">
+          <div className="modal__title">{title}</div>
           <button
-            className='button--icon-only modal__close'
-            data-rs-modal-close-trigger=''
+            className="button--icon-only modal__close"
+            data-rs-modal-close-trigger=""
             aria-label={ariaLabelClose}
-            type='button'
+            type="button"
             onClick={(event: CloseEvents) => modalClose(event)}
           >
-            <Icon iconClassName={classNames('icon icon--inline hidden--from-l icon--alternative')} iconType='close' />
-            <Icon iconClassName={classNames('icon icon--l icon--inline hidden--until-l icon--alternative')} iconType='close-30' />
+            <Icon iconClassName={classNames('icon icon--inline hidden--from-l icon--alternative')} iconType="close" />
+            <Icon iconClassName={classNames('icon icon--l icon--inline hidden--until-l icon--alternative')} iconType="close-30" />
           </button>
         </div>
-        <div className='modal__main' data-rs-modal-main=''>
-          <p className='form__header'>{content}</p>
+        <div className="modal__main" data-rs-modal-main="">
+          <p className="form__header">{content}</p>
         </div>
-        <div className='modal__footer divider' data-rs-modal-footer=''>
-          <div className='button-group button-group--full-width hidden--from-l'>
-            <Button href='#' variant='filled' fullWidth handleClick={onSubmit}>
+        <div className="modal__footer divider" data-rs-modal-footer="">
+          <div className="button-group button-group--full-width hidden--from-l">
+            <Button href="#" variant="filled" fullWidth handleClick={onSubmit}>
               {confirmButtonText}
             </Button>
-            <Button href='#' variant='plain' fullWidth handleClick={(event: CloseEvents) => modalClose(event)}>
+            <Button href="#" variant="plain" fullWidth handleClick={(event: CloseEvents) => modalClose(event)}>
               {confirmButtonText}
             </Button>
           </div>
-          <div className='button-group hidden--until-l button-group--options'>
-            <Button href='#' variant='filled' handleClick={onSubmit}>
+          <div className="button-group hidden--until-l button-group--options">
+            <Button href="#" variant="filled" handleClick={onSubmit}>
               {confirmButtonText}
             </Button>
-            <Button href='#' variant='plain' handleClick={(event: CloseEvents) => modalClose(event)}>
+            <Button href="#" variant="plain" handleClick={(event: CloseEvents) => modalClose(event)}>
               {cancelButtonText}
             </Button>
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </ModalStyle>
   );
 }
 
