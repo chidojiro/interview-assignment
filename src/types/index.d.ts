d4d2152a8c8977eee1043d32fcd314f47ab2cbe2
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-02-16 13:47:35 +0200

    RXPIP-8021: Reverted back imports of orbit libs.

---
+++ src/types/index.d.ts
@@ -0,0 +1,3 @@
+declare module '@ffw/randstad-local-orbit/js/components/collapsible';
+declare module '@ffw/randstad-local-orbit/js/components/toggable';
+declare module '@ffw/randstad-local-orbit';
