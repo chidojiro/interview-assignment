80eb1d11d3b8796ba95aec51973a5d973cb53ffd
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-02-27 16:52:57 +0200

    RXPIP-8021: Changed the import and attachment of orbit js.

--- src/components/navigation/TabBar.tsx
+++ src/components/navigation/TabBar.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { TabBar as OrbitComponent } from '@ffw/randstad-local-orbit/original/js/components/tab-bar';
 import Icon from '../Icon';
-import useOrbitComponent from '../../hooks/useOrbitComponent';
 
 type TabBarItem = {
   title: string;
   url: string;
   icon: string;
+  isActive: boolean;
 };
 
 interface TabBarProps {
@@ -14,21 +15,29 @@ interface TabBarProps {
 }
 
 function TabBar({ items = [], RouterComponent }: TabBarProps) {
-  const [ref] = useOrbitComponent('tab-bar');
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    if (!ref.current.dataset.rendered) {
+      ref.current.dataset.rendered = 'rendered';
+      new OrbitComponent(ref.current);
+    }
+  }, [items]);
 
   return (
     <div ref={ref} className="tab-bar tab-bar--icon" data-rs-tab-bar="" data-rs-tab-bar-animation-type="instant">
       {items.map((item) => {
         if (RouterComponent) {
           return (
-            <RouterComponent key={item.icon} href={item.url} className="tab-bar__item" data-rs-tab-bar-item="">
+            <RouterComponent key={item.icon} href={item.url} className={`tab-bar__item ${item.isActive ? 'active' : ''}`} data-rs-tab-bar-item="">
               <Icon iconType={item.icon} />
               {item.title}
             </RouterComponent>
           );
         }
         return (
-          <a key={item.icon} href={item.url} className="tab-bar__item" data-rs-tab-bar-item="">
+          <a key={item.icon} href={item.url} className={`tab-bar__item ${item.isActive ? 'active' : ''}`} data-rs-tab-bar-item="">
             <Icon iconType={item.icon} />
             {item.title}
           </a>
