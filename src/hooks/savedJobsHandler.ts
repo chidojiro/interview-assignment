3a390e2864e4b960176cc5a4abc19f81913767a6
Zarko Petrov <zarko.petrov@ffw.com>
2023-04-20 11:10:55 +0300

    RXPIP-9712: Heart icon - add some types.

--- src/hooks/savedJobsHandler.ts
+++ src/hooks/savedJobsHandler.ts
@@ -8,7 +8,14 @@ export interface SavedJobs {
 export type Data = {
   // Data describes a generic abstract structure of request, which can be anything.
   /* eslint-disable @typescript-eslint/no-explicit-any */
-  [id: string]: any;
+  data: {
+    content: Array<object>;
+    pageNumber: number;
+    pageSize: number;
+    totalElements: number;
+    numberOfElements: number;
+    empty: boolean;
+  };
 };
 
 // Function that checks if the current logged in user has an already uplaoded file.
