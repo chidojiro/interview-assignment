ff4e17ad305a07fc3844b041e51a5a81c74bbd01
Vasil Vasilev <vasil.vasilev@ffwagency.com>
2023-04-28 14:04:11 +0300

    RXPIP-7087: removes unnecessary exports.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,13 +1,9 @@
 import * as savedJobsHandler from './savedJobsHandler';
 import FormatFileSize from './formatFileSize';
 import * as resumeHandler from './resumeHandler';
-import useOrbitComponent from './useOrbitComponent';
-import useLibrary from './useLibrary';
 
 export {
   savedJobsHandler,
   FormatFileSize,
   resumeHandler,
-  useLibrary,
-  useOrbitComponent,
 };
