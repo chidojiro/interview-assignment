744095f0543e92d1ee18b71351f4f2fb42fee9f6
Vasil Vasilev <vasil.vasilev@ffwagency.com>
2023-04-28 13:55:02 +0300

    RXPIP-7087: exporting hooks to make them usable in the app.

---
+++ src/hooks/index.ts
@@ -0,0 +1,13 @@
+import * as savedJobsHandler from './savedJobsHandler';
+import FormatFileSize from './formatFileSize';
+import * as resumeHandler from './resumeHandler';
+import useOrbitComponent from './useOrbitComponent';
+import useLibrary from './useLibrary';
+
+export {
+  savedJobsHandler,
+  FormatFileSize,
+  resumeHandler,
+  useLibrary,
+  useOrbitComponent,
+};
