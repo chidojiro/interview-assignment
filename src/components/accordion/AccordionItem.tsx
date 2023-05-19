5ea80a9861917ad0c61c60d0a2c0de969b9c947c
radost <radostina.mangarudova@ffwagency.com>
2023-05-19 13:17:20 +0300

    RXPIP-9976: Toggable and collapsible accordion item.

--- src/components/accordion/AccordionItem.tsx
+++ src/components/accordion/AccordionItem.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import useLibrary, { Library } from "../../hooks/useLibrary";
+import React, { useEffect, useRef } from 'react';
+import { Collapsible } from '@ffw/randstad-local-orbit/js/components/collapsible';
+import { Toggable } from '@ffw/randstad-local-orbit/js/components/toggable';
 
-interface AccordionItem extends Library {
+interface AccordionItemInterface {
   children: React.ReactNode,
   title: string,
   subtitle?: string,
   expanded?: boolean,
-  /** Used to pass js Orbit library responsible for functionality. Note: This should passed on component setup so you don't have to pass it every time. */
   HeadingTag?: keyof JSX.IntrinsicElements,
   ariaLabel: string,
 }
@@ -15,52 +15,61 @@ interface AccordionItem extends Library {
  * Expanding and collapsing sections of content. See [here](https://randstad.design/components/core/accordion/)
  *
  */
-const AccordionItem = ({
+function AccordionItem({
   children,
   title,
   subtitle,
   expanded = false,
-  libs,
-  HeadingTag = "h3",
-  ariaLabel = "",
+  HeadingTag = 'h3',
+  ariaLabel = '',
   ...attr
-}: AccordionItem) => {
-  const [ref] = useLibrary<HTMLDivElement>(libs);
+}: AccordionItemInterface) {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (ref.current?.hasAttribute('data-rs-collapsible') && ref.current?.hasAttribute('data-rs-toggable') && !ref.current.hasAttribute('data-rendered')) {
+      new Collapsible(ref.current);
+      new Toggable(ref.current);
+      ref.current.dataset.rendered = 'rendered';
+    }
+  }, []);
 
   return (
     <li {...attr} className="link-list__item">
       <div
-        className={`collapsible__trigger ${expanded ? "icon__toggler--active" : ""}`}
+        className={`collapsible__trigger ${expanded ? 'icon__toggler--active' : ''}`}
         data-rs-collapsible=""
         role="button"
         aria-expanded={expanded}
         aria-label={ariaLabel}
         data-rs-toggable=""
         ref={ref}
-        data-scl="">
+        data-scl=""
+      >
         <HeadingTag className="link-list__link">
           {title}
           {subtitle && <p className="text--alternative pt-xs mb-none">{subtitle}</p>}
           <span className="hidden--from-l toggle-arrow icon">
             <svg>
-              <use xlinkHref="/src/assets/img/icons.svg#chevron-down"></use>
+              <use xlinkHref="/src/assets/img/icons.svg#chevron-down" />
             </svg>
           </span>
           <span className="hidden--until-l toggle-arrow icon icon--l">
             <svg>
-              <use xlinkHref="/src/assets/img/icons.svg#chevron-down-30"></use>
+              <use xlinkHref="/src/assets/img/icons.svg#chevron-down-30" />
             </svg>
           </span>
         </HeadingTag>
       </div>
       <div
         className="collapsible__content body-copy"
-        data-rs-collapsible-content={`${expanded ? "expanded" : ""}`}
-        aria-hidden={!expanded}>
+        data-rs-collapsible-content={`${expanded ? 'expanded' : ''}`}
+        aria-hidden={!expanded}
+      >
         <div className="collapsible__content--wrapper">{children}</div>
       </div>
     </li>
   );
-};
+}
 
 export default AccordionItem;
