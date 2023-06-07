cced283360264e35b6e70118251152cf572395cc
yoan.ivanov <yoan.ivanov@ffw.com>
2023-06-07 09:39:59 +0300

    RXPIP-7053: Add unit tests.

--- src/__tests__/components/Tag.test.tsx
+++ src/__tests__/components/Tag.test.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Tag from '../../components/Tag/Tag';
 
 describe('Tag component tests', () => {
-  test('Tag renders correctly with required props', () => {
-    function test() {
-      return 0;
-    }
+  const mockOnClick = jest.fn();
 
-    const { container } = render(
-      <Tag
-        id="tag-1"
-        title="Teamplayer met lange titel"
-        variant="secondary"
-        onClick={() => test()}
-      />,
+  beforeEach(() => {
+    mockOnClick.mockClear();
+  });
+
+  test('renders the tag with default variant and size and has required class when clicked on', () => {
+    const { getByText, getByLabelText } = render(
+      <Tag id="tag1" title="Tag 1" onClick={mockOnClick} />,
     );
-    const TagElement = container.getElementsByClassName('tag tag--remove  tag--secondary');
-    waitFor(() => expect(TagElement).toBeInTheDocument());
+
+    const tag = getByLabelText('remove');
+    const tagText = getByText('Tag 1');
+
+    expect(tag.parentElement).toHaveClass('tag--primary-7');
+    expect(tag.parentElement).toHaveClass('tag--remove');
+    expect(tagText).toBeInTheDocument();
+
+    fireEvent.click(tag);
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+    expect(tag.parentElement).toHaveClass('hide-tag');
+  });
+
+  test('renders the tag with secondary variant and small size', () => {
+    const { getByLabelText } = render(
+      <Tag id="tag2" title="Tag 2" onClick={mockOnClick} variant="secondary" size="small" />,
+    );
+
+    const tag = getByLabelText('remove');
+
+    expect(tag.parentElement).toHaveClass('tag--secondary');
+    expect(tag.parentElement).toHaveClass('tag--remove');
+    expect(tag.parentElement).toHaveClass('tag--s');
   });
 });
