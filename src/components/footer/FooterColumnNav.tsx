26dded4d9eb67ceae726340b9b092b5415ba215a
Alexander Mikhutkin <alexander.mikhutkin@ffwagency.com>
2023-06-15 12:35:34 +0300

    RXPIP-10957: Footer - fixed warning in console. Add Orbit functionality to component.

--- src/components/footer/FooterColumnNav.tsx
+++ src/components/footer/FooterColumnNav.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Collapsible } from '@ffw/randstad-local-orbit/js/components/collapsible';
+import { Toggable } from '@ffw/randstad-local-orbit/js/components/toggable';
 import Icon from '../Icon';
 
 interface FooterColumnNavProps {
@@ -11,59 +13,79 @@ export interface ColumnChildren {
   children: ColumnChildren[] | [];
 }
 
-const renderColumnItemLinks = (links: ColumnChildren[]) => {
-  if (!links || links.length === 0) {
-    return null;
-  }
-  return (
-    <ul>
-      {links.map((link) => (
-        <li key={link.url}>
-          <a href={link.url}>{link.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-const renderColumnTitles = (columnTitles: ColumnChildren[], columnId: number) => {
-  if (!columnTitles || columnTitles.length === 0) {
-    return null;
-  }
-  return (
-    <ul className="extensive-link-list" key={`column-${columnId}`}>
-      {
-        columnTitles.map((columnTitle, id) => (
-          <li className="extensive-link-list__item divider" key={`list-divider-${id}`}>
-            <div className="collapsible__trigger" data-rs-collapsible="" role="button" aria-expanded="false" data-rs-toggable="">
-              <span className="extensive-link-list__list-title">
-                {columnTitle.title}
-                {
-                  columnTitle?.children && columnTitle.children.length > 0
-                  && <Icon iconClassName="hidden--from-l icon toggle-arrow" iconType="chevron-down" />
-                }
-              </span>
-            </div>
-            <div className="collapsible__content" data-rs-collapsible-content="" aria-hidden="true">
-              { renderColumnItemLinks(columnTitle.children) }
-            </div>
-          </li>
-        ))
-      }
-    </ul>
-  );
-};
-
 export default function FooterColumnNav({ columns }: FooterColumnNavProps) {
+  const columnRefs = useRef<(HTMLDivElement | null)[][]>([]);
+
+  useEffect(() => {
+    if (columns.length) {
+      const referItems = [];
+
+      // Collect data from columns children into one array.
+      columns.forEach((column) => {
+        referItems.push(column.children);
+      });
+
+      // Assign refs to the DOM elements.
+      columnRefs.current = columnRefs.current.slice(0, referItems.length);
+
+      // Apply an Orbit functionality to the menu titles for the small screens and mobile devices.
+      // As incoming data is a two-dimensional array, we must transform it into one dimension array.
+      for (const item of columnRefs.current.flat()) {
+        if (item?.hasAttribute('data-rs-collapsible')
+          && item?.hasAttribute('data-rs-toggable')
+          && !item.hasAttribute('data-rendered')) {
+          new Collapsible(item);
+          new Toggable(item);
+          item.dataset.rendered = 'rendered';
+        }
+      }
+    }
+  }, [columns]);
+
   return (
     <div data-testid="footer-column-nav" className="footer__grid divider">
-      {
-        columns.map((column, id) => (
-          <div className="footer__column" key={column.title}>
-            { renderColumnTitles(column.children, id) }
-          </div>
-        ))
-      }
+      {columns.map((column, columnIndex) => (
+        <div className="footer__column" key={column.title}>
+          {column.children && column.children.length > 0 && (
+            <ul className="extensive-link-list">
+              {column.children.map((columnItem, itemIndex) => (
+                <li className="extensive-link-list__item divider" key={columnItem.title}>
+                  <div
+                    ref={(el) => {
+                      if (el) {
+                        columnRefs.current[columnIndex] = columnRefs.current[columnIndex] || [];
+                        columnRefs.current[columnIndex][itemIndex] = el;
+                      }
+                    }}
+                    className="collapsible__trigger"
+                    data-rs-collapsible=""
+                    data-rs-toggable=""
+                    role="button"
+                    aria-expanded="false"
+                  >
+                    <span className="extensive-link-list__list-title">
+                      {columnItem.title}
+                      {columnItem?.children && columnItem.children.length > 0
+                        && (<Icon iconClassName="hidden--from-l icon toggle-arrow" iconType="chevron-down" />)}
+                    </span>
+                  </div>
+                  <div className="collapsible__content" data-rs-collapsible-content="true" aria-hidden="true">
+                    {columnItem.children && columnItem.children.length > 0 && (
+                      <ul>
+                        {columnItem.children.map((link) => (
+                          <li key={link.title}>
+                            <a href={link.url}>{link.title}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
