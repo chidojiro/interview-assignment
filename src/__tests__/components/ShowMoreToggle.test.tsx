175c5f56e5d5b11407eb5534f57d21dc0f7fb5e0
Vasil Vasilev <vasil.vasilev@ffwagency.com>
2023-06-16 13:38:58 +0300

    RXPIP-7049: code and test improvements.

--- src/__tests__/components/ShowMoreToggle.test.tsx
+++ src/__tests__/components/ShowMoreToggle.test.tsx
@@ -36,4 +36,38 @@ describe('ShowMoreToggle', () => {
 
     expect(screen.queryByText('Show less')).toBeInTheDocument();
   });
+
+  it('Tests if items array is empty', () => {
+    const { container } = render(
+      <ShowMoreToggle
+        items={[]}
+        handleClick={() => (false)}
+        isViewAllOpen
+        translations={{
+          viewAll: `View all ${arrayOfItems.length} items`,
+          viewLess: 'Show less',
+        }}
+      />,
+    );
+    const showMoreToggle = container.querySelector('view-more-button');
+    expect(showMoreToggle).toBeNull();
+  });
+
+  it('Tests if maxItemsShown is used and equals the total items.', () => {
+    const { container } = render(
+      <ShowMoreToggle
+        items={arrayOfItems}
+        handleClick={() => (false)}
+        isViewAllOpen
+        translations={{
+          viewAll: `View all ${arrayOfItems.length} items`,
+          viewLess: 'Show less',
+        }}
+        maxItemsShown={5}
+      />,
+    );
+
+    const showMoreToggle = container.querySelector('view-more-button');
+    expect(showMoreToggle).toBeNull();
+  });
 });
--- src/components/ShowMoreToggle/index.tsx
+++ src/components/ShowMoreToggle/index.tsx
@@ -1,15 +1,5 @@
 import React from 'react';
-
-export interface ShowMoreToggleInterface {
-  items: unknown[] | [];
-  handleClick: () => void;
-  translations: {
-    viewAll: string;
-    viewLess: string;
-  }
-  isViewAllOpen: boolean;
-  maxItemsShown?: number;
-}
+import { ShowMoreToggleInterface } from './ShowMoreToggle.types';
 
 function ShowMoreToggle({
   items, handleClick, translations, isViewAllOpen, maxItemsShown = 3,
@@ -19,18 +9,22 @@ function ShowMoreToggle({
   return (
     // Anchor tag uses orbit for styling.
     // eslint-disable-next-line jsx-a11y/anchor-is-valid,react/react-in-jsx-scope
-    <a
-      href="#"
-      onClick={(e) => {
-        e.preventDefault();
-        handleClick();
-      }}
-      aria-label={translations.viewAll}
-      className="view-more-link mt-m"
-    >
-      {!isViewAllOpen
-        ? translations.viewAll : translations.viewLess }
-    </a>
+    <div className="text-center mt-m">
+      {/* Anchor tag uses orbit for styling. */}
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid,react/react-in-jsx-scope */}
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          handleClick();
+        }}
+        aria-label={translations.viewAll}
+        className="view-more-button"
+      >
+        {!isViewAllOpen
+          ? translations.viewAll : translations.viewLess }
+      </a>
+    </div>
   );
 }
 
