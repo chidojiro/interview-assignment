12ea4ed28e5edf6869d49b55fdae4d4888528327
SpasSimeonov <spas.simeonov@ffwagency.com>
2023-06-19 10:05:05 +0300

    RXPIP-10996: Adding fixes from peer review of the ApplicationProcessCard.

--- src/components/ApplicationProcess/ApplicationProcessCard.tsx
+++ src/components/ApplicationProcess/ApplicationProcessCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 import { ApplicationProcessCardProp } from './ApplicationProcessCard.types';
 
 function ApplicationProcessCard({
@@ -13,12 +14,12 @@ function ApplicationProcessCard({
   return (
     <li className="blog-overview__item">
       <div className="banner" data-rs-carousel-image="data-rs-carousel-image">
-        <div className={`banner__wrapper ${cardColor}`}>
+        <div className={cn('banner__wrapper', `${cardColor}`)}>
           <div className="banner__media media-block">
-            <img src={`/jobs-app/src/assets/img/application-process/${cardImage}.svg`} alt={accessibilityItemNumber} />
+            <img src={cardImage} alt={accessibilityItemNumber} />
           </div>
           <div className="banner__content content-block">
-            <span aria-hidden="true" className={`badge badge--l ${badgeColor}`}>
+            <span aria-hidden="true" className={cn('badge badge--l', `${badgeColor}`)}>
               {itemNumber}
             </span>
             <h3 className="content-block__title">{cardTitle}</h3>
