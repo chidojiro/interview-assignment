9ce71ecf8ddce3b6304f1020fb6d559d41a9e036
Radoslav Radoychev <radoslav.radoychev@ffwagency.com>
2023-06-30 14:44:16 +0300

    RXPIP-10763: Added tests for the Preloader component.

---
+++ src/__tests__/components/loaders/Preloader.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // For DOM assertions
+import Preloader from '../../../components/loaders/Preloader';
+
+describe('Preloader component', () => {
+  // Reusable data variables
+  const buttonClass = 'button button--preloader button--m button--plain';
+  const dotsClass = 'dots';
+
+  it('should render correctly', () => {
+    const { getByTestId, getByText, getByRole } = render(<div data-testid="test-id"><Preloader /></div>);
+
+    const preloaderContainer = getByTestId('test-id');
+    expect(preloaderContainer).toBeInTheDocument();
+
+    const button = getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+    expect(button).toHaveClass(buttonClass);
+
+    const dots = getByText('', { selector: `.${dotsClass}` });
+    expect(dots).toBeInTheDocument();
+  });
+
+  it('should have the correct dots element', () => {
+    const { getByText } = render(<div data-testid="test-id"><Preloader /></div>);
+    const dots = getByText('', { selector: `.${dotsClass}` });
+    expect(dots).toHaveClass(dotsClass);
+  });
+});
