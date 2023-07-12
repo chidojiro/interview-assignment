79ae9b9bc0e9b98e97ec799bdc5f0e238f06e43d
Radoslav Radoychev <radoslav.radoychev@ffwagency.com>
2023-07-12 13:38:21 +0300

    RXPIP-11310: Fixed JobCard tests after merge. Moved SummaryCard outside the JobCard test folder.

--- src/__tests__/components/cards/SummaryCard/SummaryCard.test.tsx
+++ src/__tests__/components/cards/SummaryCard/SummaryCard.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import SummaryCard from '../../../../../components/cards/SummaryCard';
+import SummaryCard from '../../../../components/cards/SummaryCard';
 
 describe('SummaryCard', () => {
   test('renders the component with count', () => {
