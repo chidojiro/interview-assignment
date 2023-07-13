ee1777b759382a699788cce267c8dce0fa7e03b2
Stefan Stefanov <stefan.stefanov@randstad.com>
2023-07-13 09:24:47 +0300

    Fix failing unit test for UploadField

--- src/__tests__/components/forms/UploadField.test.tsx
+++ src/__tests__/components/forms/UploadField.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import UploadField from '../../../components/forms/UploadField';
 
 describe('UploadField component tests', () => {
-  test('UploadField renders correctly with its props', () => {
+  test('UploadField renders correctly with its props', async () => {
     const { container } = render(
       <UploadField
         name="resume"
@@ -37,17 +37,23 @@ describe('UploadField component tests', () => {
       />,
     );
 
-    const uploadField = container.querySelector('.upload');
-    const uploadAddFiles = container.querySelector('.upload__add');
-    const orDropFilesHere = container.querySelector('.hidden--until-l');
-    const uploadContent = container.querySelector('.upload__content');
+    const preloader = container.querySelector('button.button--preloader');
 
-    expect(uploadField).toBeInTheDocument();
-    expect(uploadAddFiles).toBeInTheDocument();
-    expect(uploadAddFiles).toHaveTextContent('add files');
-    expect(orDropFilesHere).toBeInTheDocument();
-    expect(orDropFilesHere).toHaveTextContent('or drop files here');
-    expect(uploadContent).toBeInTheDocument();
-    expect(uploadContent).toHaveTextContent('{allowedFiles} / max {maxFileSize} mb');
+    expect(preloader).toBeInTheDocument();
+
+    await waitFor(() => {
+      const uploadField = container.querySelector('.upload');
+      const uploadAddFiles = container.querySelector('.upload__add');
+      const orDropFilesHere = container.querySelector('.hidden--until-l');
+      const uploadContent = container.querySelector('.upload__content');
+
+      expect(uploadField).toBeInTheDocument();
+      expect(uploadAddFiles).toBeInTheDocument();
+      expect(uploadAddFiles).toHaveTextContent('add files');
+      expect(orDropFilesHere).toBeInTheDocument();
+      expect(orDropFilesHere).toHaveTextContent('or drop files here');
+      expect(uploadContent).toBeInTheDocument();
+      expect(uploadContent).toHaveTextContent('{allowedFiles} / max {maxFileSize} mb');
+    });
   });
 });
