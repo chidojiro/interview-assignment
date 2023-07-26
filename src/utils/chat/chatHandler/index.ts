9277fe573bc28a6f8e6930d547dbc334b441a454
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-07-26 11:13:29 +0300

    RXPIP-10918: Reverted chatApi and chatHandler to be in the UI lib.

---
+++ src/utils/chat/chatHandler/index.ts
@@ -0,0 +1,40 @@
+import { AxiosResponse } from 'axios';
+import { ChatApi } from '../chatApi';
+
+export type Data = {
+  // Data describes a generic abstract structure of request, which can be anything.
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  [id: string]: any;
+};
+
+export interface ChatConversation {
+  [id: string]: any;
+}
+
+const initChat = async (
+  chatId: string,
+  chatApiKey: string,
+  chatApiUrl: string,
+  applicationId: string,
+  chatTimezone: string,
+  chatDateFormat: string,
+  locale: string,
+): Promise<ChatConversation | undefined> => {
+  const chatApi = new ChatApi(chatId, chatApiKey, chatApiUrl);
+
+  const response = await chatApi
+    .initChat<Data, AxiosResponse<ChatConversation>>(chatApiKey, applicationId, chatTimezone, chatDateFormat, locale)
+    .catch((err) => {
+      // Needed logging for error.
+      // eslint-disable-next-line no-console
+      console.error('ChatInit Error: ', err);
+      return undefined;
+    });
+  if (response) {
+    return response?.data;
+  }
+
+  return undefined;
+};
+
+export { initChat };
