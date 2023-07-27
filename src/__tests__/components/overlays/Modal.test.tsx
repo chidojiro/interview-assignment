f5997b022009159da325dbe8e0bff2ce717e1519
ban6ka <ivan.goncharenko@ffwagency.com>
2023-07-27 14:13:06 +0300

    RXPIP-11352: provide an option to avoid window history changes in Modal component.

--- src/__tests__/components/overlays/Modal.test.tsx
+++ src/__tests__/components/overlays/Modal.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { useState } from 'react';
+import { fireEvent, render } from '@testing-library/react';
 import Modal from '../../../components/overlays/Modal';
 
+function ComponentWithModal(props: { [key: string]: string | boolean }) {
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <>
+      <button type="button" onClick={() => setOpenModal(true)}>Open modal</button>
+      {openModal && (
+        <Modal title="Modal title" {...props}>Modal Content</Modal>
+      )}
+    </>
+  );
+}
+
 describe('Modal component', () => {
-  it('Modal is rendered with all of its props', () => {
+  const { pushState } = window.history;
+  beforeEach(() => {
+    window.history.pushState = pushState;
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be rendered with all of its props', () => {
     const { container } = render(
       <Modal
         title="Modal title"
@@ -29,5 +49,25 @@ describe('Modal component', () => {
     expect(modalFooter).toHaveTextContent('Modal footer');
     expect(modalElementFirstChild).toHaveClass('modal__dialog test-variant-brand');
     expect(modalFooterParent).toHaveClass('modal__footer divider divider--top');
+  });
+
+  it('should change window history whenever it is opened or closed', () => {
+    window.history.pushState = jest.fn();
+    const { container, getByText } = render(<ComponentWithModal />);
+
+    fireEvent.click(getByText('Open modal'));
+    expect(window.history.pushState).toHaveBeenCalledWith({}, '', '/?modal_open=modal');
+    fireEvent.click(container.querySelector('button.modal__close') as HTMLButtonElement);
+    expect(window.history.pushState).toHaveBeenCalledWith({}, '', '/');
+  });
+
+  it('should not change window history once opened or closed when disableBrowserHistory is set to true', () => {
+    window.history.pushState = jest.fn();
+    const { container, getByText } = render(<ComponentWithModal disableBrowserHistory />);
+
+    fireEvent.click(getByText('Open modal'));
+    expect(window.history.pushState).not.toHaveBeenCalled();
+    fireEvent.click(container.querySelector('button.modal__close') as HTMLButtonElement);
+    expect(window.history.pushState).not.toHaveBeenCalled();
   });
 });
