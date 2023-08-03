19c483a164fc18e57c96e487ca8b4ee8c9c2ff51
nikolaystanev <nikolay.stanev@ffwagency.com>
2023-08-03 16:46:15 +0300

    RXPIP-10919: Added added handlechange for the multiselect fields.

--- src/components/chat/ChatMultiSelect/index.tsx
+++ src/components/chat/ChatMultiSelect/index.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChatMultiSelectProps } from './ChatMultiSelect.types';
 import TagCheckbox from '../../tags/TagCheckbox';
+import { ConversationMultiSelectItem } from '../../../utils/chatApi/types';
 
 function ChatMultiSelect({ items }: ChatMultiSelectProps) {
+  const [selectedItems, setSelectedItems] = useState<Array<ConversationMultiSelectItem>>([]);
+
   useEffect(() => {
     const multiSelectButtons = document.querySelector('[data-rs-chat-tags]');
     if (!multiSelectButtons) return;
 
     if ((window as Window).orbit && (window as Window).orbit?.chatInstance) {
-      console.log(window.orbit?.chatInstance);
-      (window as Window).orbit?.chatInstance?.handleTagDialogButtons();
+      (window as Window).orbit?.chatInstance?.tagHandler();
     }
   }, []);
 
@@ -18,6 +20,16 @@ function ChatMultiSelect({ items }: ChatMultiSelectProps) {
       <div className="chat__content chat__content--bot">
         {items.map((item) => (
           <TagCheckbox
+            handleChange={() => {
+              const found = selectedItems.findIndex((predicated) => predicated.param === item.param);
+              if (found !== -1) {
+                const selectedItemsCopy = [...selectedItems];
+                selectedItemsCopy.splice(found, 1);
+                setSelectedItems(selectedItemsCopy);
+              } else {
+                setSelectedItems((oldItems) => [...oldItems, item]);
+              }
+            }}
             key={item.param}
             className="mb-xxs mr-xxs"
             value={item.param as string}
