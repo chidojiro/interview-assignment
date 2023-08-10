bc0ae7a39c221cbc0d5ca2de7ec305b397355b23
Radoslav Radoychev <radoslav.radoychev@ffwagency.com>
2023-08-10 11:13:38 +0300

    RXPIP-11896: Simplified the test and applied DRY principle.

--- src/__tests__/utils/getKeyCodeOnKeyDownEvent.test.tsx
+++ src/__tests__/utils/getKeyCodeOnKeyDownEvent.test.tsx
@@ -1,73 +1,26 @@
 import { getKeyCodeOnKeyDownEvent } from '../../utils';
 
 describe('getKeyCodeOnKeyDownEvent', () => {
-  it('should return "Enter" for key code 13', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 13 });
-    Object.defineProperty(event, 'code', { value: 'Enter' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('Enter');
-  });
+  it('should return the correct key code value', () => {
+    const testCases = [
+      { keyCode: 13, code: 'Enter' },
+      { keyCode: 38, code: 'ArrowUp' },
+      { keyCode: 40, code: 'ArrowDown' },
+      { keyCode: 8, code: 'Backspace' },
+      { keyCode: 9, code: 'Tab' },
+      { keyCode: 27, code: 'Escape' },
+      { keyCode: 33, code: 'PageUp' },
+      { keyCode: 34, code: 'PageDown' },
+      { keyCode: 32, code: 'Space' },
+      { keyCode: 123, code: undefined },
+      { keyCode: undefined, code: 'KeyA' },
+      { keyCode: null, code: undefined },
+    ];
 
-  it('should return "ArrowUp" for key code 38', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 38 });
-    Object.defineProperty(event, 'code', { value: 'ArrowUp' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('ArrowUp');
-  });
-
-  it('should return "ArrowDown" for key code 40', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 40 });
-    Object.defineProperty(event, 'code', { value: 'ArrowDown' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('ArrowDown');
-  });
-
-  it('should return "Backspace" for key code 8', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 8 });
-    Object.defineProperty(event, 'code', { value: 'Backspace' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('Backspace');
-  });
-
-  it('should return "Tab" for key code 9', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 9 });
-    Object.defineProperty(event, 'code', { value: 'Tab' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('Tab');
-  });
-
-  it('should return "Escape" for key code 27', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 27 });
-    Object.defineProperty(event, 'code', { value: 'Escape' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('Escape');
-  });
-
-  it('should return "PageUp" for key code 33', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 33 });
-    Object.defineProperty(event, 'code', { value: 'PageUp' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('PageUp');
-  });
-
-  it('should return "PageDown" for key code 34', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 34 });
-    Object.defineProperty(event, 'code', { value: 'PageDown' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('PageDown');
-  });
-
-  it('should return "Space" for key code 32', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 32 });
-    Object.defineProperty(event, 'code', { value: 'Space' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('Space');
-  });
-
-  it('should return undefined for unknown key code', () => {
-    const event = new KeyboardEvent('keydown', { keyCode: 123 }); // Unknown key code
-    Object.defineProperty(event, 'code', { value: undefined });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBeUndefined();
-  });
-
-  it('should return event.code when defined', () => {
-    const event = new KeyboardEvent('keydown');
-    Object.defineProperty(event, 'code', { value: 'KeyA' });
-    expect(getKeyCodeOnKeyDownEvent(event)).toBe('KeyA');
-  });
-
-  it('should return undefined for null event', () => {
-    expect(getKeyCodeOnKeyDownEvent(null as any)).toBeUndefined();
+    testCases.forEach((testCase) => {
+      const event = new KeyboardEvent('keydown', { keyCode: testCase.keyCode as number });
+      Object.defineProperty(event, 'code', { value: testCase.code });
+      expect(getKeyCodeOnKeyDownEvent(event)).toBe(testCase.code);
+    });
   });
 });
