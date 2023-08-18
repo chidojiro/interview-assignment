307a5dc115ebc078e5bcf154bf0d8a5f3d1ed3c4
nikolaystanev <nikolay.stanev@ffwagency.com>
2023-08-18 14:07:19 +0300

    RXPIP-11959: Fixed the multiselect text bubble.

--- src/hooks/chat/useHandleChatReplies/index.tsx
+++ src/hooks/chat/useHandleChatReplies/index.tsx
@@ -30,6 +30,10 @@ function useHandleChatReplies(
   const selectedItems = useRef<Array<ConversationMultiSelectItem>>([]);
   const multiSelectData = useRef<ConversationMultiSelect>(initalMultiselectData);
   const multiSelectResponse = useRef<ContinueResponse>();
+  /**
+   * This state will control the multiselect text bubble simulation.
+   */
+  const [showTextBubble, setShowTextBubble] = useState(false);
 
   const clearMultiSelect = () => {
     if (!replyLoading) {
@@ -39,37 +43,14 @@ function useHandleChatReplies(
   };
 
   useEffect(() => {
-    if (!replyLoading && multiSelectResponse.current && window && window.orbit && window.orbit.chatInstance && window.orbit.chatInstance.textarea) {
+    if (showTextBubble && multiSelectResponse.current && window && window.orbit && window.orbit.chatInstance && window.orbit.chatInstance.textarea) {
       (window.orbit.chatInstance.textarea as HTMLTextAreaElement).value = selectedItems.current.map((r) => r.text).join(', ');
       window.orbit.chatInstance.userInputToSpeechBubble();
+      setShowTextBubble(false);
 
       handleContinueResponse(ContinueRequestType.QUICK_SUGGEST, multiSelectResponse.current, setReplies);
-      selectedItems.current = [];
-      multiSelectResponse.current = undefined;
-      multiSelectData.current = initalMultiselectData;
     }
-    /**
-     * We are unable to have the exaustive-deps eslint rule here because this is essentially a hack to simulate
-     * a user text input based of the selected multiselect text values.
-     *
-     * Essentially because we are combining vanilla js + react js we need to wait for the reply loading to be false,
-     * which signals that the response is ready to used by the replies for rendering fresh reply components @see handleContinueResponse, useHandleReplyComponents.
-     *
-     * Firstly we need to make sure that the window.orbit.chatInstance.userInputToSpeechBubble() function called
-     * and the text reply is properly rendered on the screen. This is important to call first, because otherwise it
-     * creates a desync and the front-end renders the text reply after the actual replies from the response, which we don't want
-     * as this breaks the user experience. After this done, the handleContinueResponse called, which basically updates the replies state, which in turn will render new reply components.  @see handleContinueResponse, useHandleReplyComponents.
-     *
-     * This way the replies and text input are properly synced.
-     *
-     * TLDR;
-     * We need to keep eye for the replyLoading state, other states are either unessecary or create rendering loops,
-     * which can't fixed as this is a hack and would create more hacks in turn.
-     *
-     *
-     */
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [replyLoading]);
+  }, [setReplies, showTextBubble]);
 
   const handleSubmitMultiSelect = () => {
     if (!handleMultiSelectSubmit) return;
@@ -77,6 +58,8 @@ function useHandleChatReplies(
     handleMultiSelectSubmit(multiSelectData.current, selectedItems.current).then((response) => {
       multiSelectResponse.current = response;
       setReplyLoading(false);
+      setShowTextBubble(true);
+
       return true;
     }).catch((error) => {
       // We need to log the error.
