5c32857d117419949ef5b9f84c06c0fa4a08dc3e
Stefan Stefanov <stefan.stefanov@randstad.com>
2023-08-22 17:14:07 +0300

    RXPIP-11900: Code formatting changes

--- src/utils/auth/getCookieOptions/index.ts
+++ src/utils/auth/getCookieOptions/index.ts
@@ -1,11 +1,8 @@
 import type { CookieSerializeOptions } from 'cookie';
 
-// process.env.NEXT_PUBLIC_ID_TOKEN_ALLOW_SUBDOMAINS === 'true'
-
 export default function getCookieOptions(shareIdTokenAcrossSubdomains: boolean, cookiesExtraOptions: CookieSerializeOptions = {}) {
   let cookieOptions: CookieSerializeOptions = cookiesExtraOptions ? { ...cookiesExtraOptions } : {};
 
-  // @FIXME: Usage of process.env here is a problem with pending resolution!
   if (shareIdTokenAcrossSubdomains && typeof window !== 'undefined') {
     const appHostname = window.location.hostname;
     const parts = appHostname.split('.');
