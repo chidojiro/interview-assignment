1418f1ac9c05f7f1b8d66bb83b62e01374009c7b
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-08-24 09:11:51 +0300

    RXPIP-12129: Fixing issue with multiple reply. Added disabled state for deselect and submit buttons.

--- src/utils/chat/checkForMultiSelects/index.ts
+++ src/utils/chat/checkForMultiSelects/index.ts
@@ -1,10 +1,11 @@
 const checkForMultiSelects = () => {
   const multiSelects = document.querySelectorAll('[data-rs-chat-tags]');
   // Remove any multiselect answers.
-  if (!!multiSelects && multiSelects.length > 0) {
+  if (!!multiSelects && multiSelects.length > 0 && window && window.orbit && window.orbit.chatInstance) {
     const deselectButton = document.querySelector('[data-rs-chat-tags-deselect-button]') as HTMLButtonElement;
     deselectButton.click();
     multiSelects[0].remove();
+    window.orbit?.chatInstance.multiSelectSubmitButton();
   }
 };
 
