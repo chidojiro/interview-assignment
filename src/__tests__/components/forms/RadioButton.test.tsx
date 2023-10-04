a88f433c678ea23436ad96f4b32cba68883aab17
Nikolay Marinov <nikolay.marinov@ffwagency.com>
2023-10-04 08:31:43 +0300

    RXPIP-10285: Corrected spacing.

--- src/__tests__/components/forms/RadioButton.test.tsx
+++ src/__tests__/components/forms/RadioButton.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor ,screen, fireEvent } from '@testing-library/react';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
 import RadioButton from '../../../components/forms/RadioButton';
 import Stackable from '../../../components/forms/Stackable';
 
