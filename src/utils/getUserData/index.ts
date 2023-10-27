ff8500723291cdd9eff57f7b24a9b83f288092b7
Vasil Vasilev <vasil.vasilev@ffwagency.com>
2023-10-27 16:13:54 +0300

    RXPIP-13285: removes userState from localStorage.

--- src/utils/getUserData/index.ts
+++ src/utils/getUserData/index.ts
@@ -1,6 +1,10 @@
 import { PersistData } from './types';
+import { getCookie } from 'cookies-next';
 
 function getUserData(): PersistData {
+  const refreshToken = getCookie('RefreshToken');
+  const accessToken = getCookie('AccessToken');
+
   if (typeof window === 'undefined') {
     return {
       loginStatus: false,
@@ -12,13 +16,25 @@ function getUserData(): PersistData {
   const data = JSON.parse(localStorage.getItem('userState') || '{}');
 
   let loginStatus = false;
-  if (data?.currentUser && data?.loginStatus) {
-    const { currentUser, savedJobs } = data;
-    loginStatus = data.loginStatus;
-    return { currentUser, loginStatus, savedJobs };
+  if (!refreshToken || !accessToken) {
+    if (data?.currentUser && data?.loginStatus) {
+      const {currentUser, savedJobs} = data;
+      loginStatus = data.loginStatus;
+      return {currentUser, loginStatus, savedJobs};
+    }
+
+    return { loginStatus, savedJobs: data.savedJobs };
   }
 
-  return { loginStatus, savedJobs: data.savedJobs };
+  if (data && data !== '{}') {
+    localStorage.removeItem('userState');
+  }
+  return {
+    loginStatus: false,
+    savedJobs: {
+      totalElements: 0,
+    },
+  };
 }
 
 export default getUserData;
