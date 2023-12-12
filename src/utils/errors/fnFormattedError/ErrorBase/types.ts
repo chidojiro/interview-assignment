dbff26928a5e649188e19574b3325d170356200e
nikolaystanev <nikolay.stanev@ffwagency.com>
2023-12-12 15:03:16 +0200

    RXPIP-13737: Added comments

--- src/utils/errors/fnFormattedError/ErrorBase/types.ts
+++ src/utils/errors/fnFormattedError/ErrorBase/types.ts
@@ -10,10 +10,25 @@ export type StatusCodeError = {
 };
 
 export type BaseError = {
+  /**
+   * The error type.
+   *
+   * There are 3 types - UNKNOWN, GENERAL and VALIDATION.
+   *
+   * - VALIDATION - this error type signals that the error is of type validation (for example a wrong email format).
+   * - GENERAL - this error type signals that something more general happened. (for example wrong email or password (on login))
+   * - UNKNOWN - this error types signals that something unexpected happened, and we are not prepared to handle it
+   *             (GDS responds to us with an error of unknown format, or a 500 status code)
+   */
   type: ErrorType;
+  // The error status code.
   statusCode?: number;
+  // The error message.
   message?: string;
+  // The error stack.
   stack?: string;
+  // The error context.
   context?: string;
+  // Flag if the error is a formatted error, used for identifying.
   isFormattedError: boolean;
 };
