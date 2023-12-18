31f2b5710f17c277133ae211d7d92d39788bfcca
Stefan Stefanov <stefan.stefanov@randstad.com>
2023-12-18 17:32:28 +0200

    RXPIP-13736: Better comments on fix for RXPIP-13921 (Auth token issue)

--- src/utils/auth/authStorage/willIdTokenExpireIn/index.ts
+++ src/utils/auth/authStorage/willIdTokenExpireIn/index.ts
@@ -2,7 +2,12 @@ import jwtDecode, { InvalidTokenError } from 'jwt-decode';
 
 function idTokenExpiresAt(idToken: string): Date {
   try {
+    // Try to decode header first. This will throw an error if token header is not valid, which
+    // in turn will force IdToken refresh.
     jwtDecode(idToken, { header: true });
+
+    // Decode the token payload in order to get token's expiration time.
+    // (this will NOT throw error even if token's header is not valid)
     const decoded: { exp: number } = jwtDecode(idToken);
     return new Date(decoded.exp * 1000); // exp is in seconds
   } catch (ex) {
