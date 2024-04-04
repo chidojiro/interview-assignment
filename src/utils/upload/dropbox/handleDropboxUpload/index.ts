2098f3edb55ceab8f57dbb2afd3b94e9821c7f99
nikolaystanev <nikolay.stanev@ffwagency.com>
2024-04-04 17:18:28 +0300

    RXPIP-14902: Fixed the issue with the safari cache + the txt file not being supported.

--- src/utils/upload/dropbox/handleDropboxUpload/index.ts
+++ src/utils/upload/dropbox/handleDropboxUpload/index.ts
@@ -6,9 +6,15 @@ import triggerDataTransfer from '../../triggerDataTransfer';
 async function handleDropboxUpload(files: Array<DropboxFile>, inputRef: React.RefObject<HTMLInputElement>) {
   const filesToUpload: Array<File> = [];
   await Promise.all(files.map(async (file) => {
-    const response = await axios.get(file.link);
+    const response = await axios.get(file.link, {
+      // We will always include the timestamp as a query param, as on Safari we have a bug where the request cached
+      // and when we hit cache we are always getting 404.
+      params: {
+        t: new Date().getTime(),
+      },
+    });
     // We will get the mime-type from the response headers, as is not available from the dropbox file callback.
-    filesToUpload.push(new File([response.data], file.name, { type: response.headers['content-type'] }));
+    filesToUpload.push(new File([response.data], file.name, { type: response.headers['content-type'].split(';')[0] }));
   }));
 
   triggerDataTransfer(filesToUpload, inputRef);
